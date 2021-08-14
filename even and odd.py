1efae1feaba572196d290215e5512924d09c89df
Anh Tran Nguyen Hai <anhtrannh@nashtechglobal.com>
2021-08-14 16:26:45 +0700

    even and odd

---
+++ even and odd.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 14 16:20:24 2021
+
+@author: anhtrannh
+"""
+
+num = input("Enter your number:")
+num_ = int(num)
+if num_ % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
