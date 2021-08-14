b3639f9adc3f4229ed8807df5fbef031aee4bf88
Anh Tran Nguyen Hai <anhtrannh@nashtechglobal.com>
2021-08-14 16:18:48 +0700

    random and draw

---
+++ random and draw.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 14 16:15:32 2021
+
+@author: anhtrannh
+"""
+
+import turtle
+import random
+ 
+number = random.uniform(0, 3)
+intNumber = int(number)
+ 
+wn = turtle.Screen()
+wn.bgcolor("black")
+wn.title("Circle")
+ 
+ball = turtle.Turtle()
+ball.shape('circle')
+ 
+if intNumber < 1:
+    ball.color('green')
+elif intNumber < 2:
+    ball.color('yellow')
+elif intNumber < 3:
+    ball.color('red')
+turtle.done()
